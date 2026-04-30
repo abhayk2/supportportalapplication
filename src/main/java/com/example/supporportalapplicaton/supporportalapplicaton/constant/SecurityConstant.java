b0a98d2e63e265abe2b059bb9c335dd78ceba682
ABHAY <abhaykum222@gmail.com>
2026-04-30 23:18:51 +0530

    Exception Handling code added

--- src/main/java/com/example/supporportalapplicaton/supporportalapplicaton/constant/SecurityConstant.java
+++ src/main/java/com/example/supporportalapplicaton/supporportalapplicaton/constant/SecurityConstant.java
@@ -11,5 +11,6 @@ public class SecurityConstant {
     public static final String FORBIDDEN_MESSAGE = "You don't have permission to access this resource";
     public static final String ACCESS_DENIED_MESSAGE = "Access Denied";
     public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
-    public static final String[]  PUBLIC_URLS = {"/user/login","/user/register","/user/resetpassword/**","/user/image/**"};
+    //public static final String[]  PUBLIC_URLS = {"/user/login","/user/register","/user/resetpassword/**","/user/image/**"};
+    public static final String[] PUBLIC_URLS = {"**"};
 }
--- src/main/java/com/example/supporportalapplicaton/supporportalapplicaton/resources/UserResource.java
+++ src/main/java/com/example/supporportalapplicaton/supporportalapplicaton/resources/UserResource.java
@@ -1,16 +1,17 @@
 package com.example.supporportalapplicaton.supporportalapplicaton.resources;
 
-import com.example.supporportalapplicaton.supporportalapplicaton.domain.User;
+import com.example.supporportalapplicaton.supporportalapplicaton.exception.domain.EmailExistsException;
+import com.example.supporportalapplicaton.supporportalapplicaton.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
-public class UserResource {
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
-    public String show(){
-        return "application works";
+    public String show() throws EmailExistsException {
+        throw new EmailExistsException("This email is already in taken");
     }
 }
