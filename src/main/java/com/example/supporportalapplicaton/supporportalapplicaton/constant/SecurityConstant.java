5571a93239f18e6342c20237044c562af8d08ae1
ABHAY <abhaykum222@gmail.com>
2026-05-03 22:28:01 +0530

    authentication jwt

--- src/main/java/com/example/supporportalapplicaton/supporportalapplicaton/constant/SecurityConstant.java
+++ src/main/java/com/example/supporportalapplicaton/supporportalapplicaton/constant/SecurityConstant.java
@@ -3,7 +3,7 @@ package com.example.supporportalapplicaton.supporportalapplicaton.constant;
 public class SecurityConstant {
     public static final long EXPIRATION_TIME = 432_000_000;//5 days
     public static final String TOKEN_PREFIX = "Bearer ";
-//    public static final String JWT_TOKEN_HEADER = "JWT-Token";
+    public static final String JWT_TOKEN_HEADER = "JWT-Token";
     public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
     public static final String GET_ARRAYS_LLC = "Get arrays LLC";
     public static final String GET_ARRAYS_ADMINISTRATION = "User Management Portal";
