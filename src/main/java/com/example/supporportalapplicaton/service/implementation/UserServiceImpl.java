0f150cbe4407cb8d7edde0227250e5a0a5d09131
ABHAY <abhaykum222@gmail.com>
2026-05-07 10:12:43 +0530

    changes

--- src/main/java/com/example/supporportalapplicaton/service/implementation/UserServiceImpl.java
+++ src/main/java/com/example/supporportalapplicaton/service/implementation/UserServiceImpl.java
@@ -55,7 +55,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             userRepository.save(userDetails);
             UserPrincipal userPrincipal = new UserPrincipal(userDetails);
             LOGGER.info("User found by username: {}", username);
-            return userPrincipal; // fix
+            return userPrincipal;
         }
     }
 
